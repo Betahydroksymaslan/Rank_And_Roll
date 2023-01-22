195242c72f7b186cebe9b45ab0663a4826706b15
BetaHydroksyMaslan <k.buchwald@webwizards.pl>
2023-01-22 21:45:18 +0100

    init app with firebase auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  typescript: {
+    strict: true,
+  },
 
-})
+  publicRuntimeConfig: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+  },
+  privateRuntimeConfig: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+  },
+});
